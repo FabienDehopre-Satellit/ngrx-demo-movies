e1767333d752bf96807c422610f4ed00791f3418
Fabien Dehopré <fabien@dehopre.com>
2023-01-30 17:58:06 +0100

    Revert "install tailwindcss"
    This reverts commit cfa32d11261cf503cbac1a3fee98b440ae0d5f17.
    
    ; Conflicts:
    ;	angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ngrx-demo';

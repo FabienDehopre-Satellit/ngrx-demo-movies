3c38837c916f3fcd0eb0cad22c3537c612146014
Fabien Dehopré <fabien.dehopre@external.equans.com>
2023-01-30 22:16:29 +0100

    create demo components + design search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {SearchMoviesBoxComponent} from "./search-movies-box/search-movies-box.component";
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [
+    MatToolbarModule,
+    SearchMoviesBoxComponent,
+
+  ],
 })
 export class AppComponent {
-  title = 'ngrx-demo';
+
 }
---
+++ src/app/search-movies-box/search-movies-box.component.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {MatIconModule} from "@angular/material/icon";
+
+@Component({
+  standalone: true,
+  selector: 'app-search-movies-box',
+  templateUrl: './search-movies-box.component.html',
+  styleUrls: ['./search-movies-box.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    NgIf,
+    MatIconModule
+  ],
+})
+export class SearchMoviesBoxComponent {
+  value = '';
+}

c10e5b2b5da5425aee62dfd1fa7d5d77e208d3f6
Fabien Dehopré <fabien@dehopre.com>
2023-01-31 11:20:33 +0100

    enable routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from "@angular/material/toolbar";
-import {SearchMoviesBoxComponent} from "./search-movies-box/search-movies-box.component";
+import {SearchMoviesBoxComponent} from "./movies/components/search-movies-box/search-movies-box.component";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [
-    MatToolbarModule,
-    SearchMoviesBoxComponent,
+    imports: [
+        MatToolbarModule,
+        SearchMoviesBoxComponent,
+        RouterOutlet,
 
-  ],
+    ],
 })
 export class AppComponent {
 
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import {bootstrapApplication} from "@angular/platform-browser";
 import {AppComponent} from "./app/app.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
+import {provideRouter} from "@angular/router";
+import {APP_ROUTES} from "./app/app.routes";
+import {provideHttpClient} from "@angular/common/http";
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserAnimationsModule)]
+  providers: [
+    importProvidersFrom(BrowserAnimationsModule),
+    provideRouter(APP_ROUTES),
+    provideHttpClient()
+  ]
 }).catch((err) => console.error(err));
